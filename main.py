8a79f1915d4ef8b9576629c21c23fa58a0ada88b
Fayash30 <mohamedfayash35@gmail.com>
2024-06-29 12:03:46 +0530

    .env updated

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
- 
+
+from dotenv import load_dotenv
+import os
 from bs4 import BeautifulSoup
 import requests,openpyxl
 
+web_url = os.getenv('URL')
+
 # try:
-#     response = requests.get("https://www.imdb.com/chart/top/")
+#     response = requests.get(web_url)
